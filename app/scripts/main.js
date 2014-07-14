87bdaba257ae43b95c2d082bdd9982a176142ca2
zena <e.nosazena@gmail.com>
2014-07-14 15:48:53 -0400

    first commit updated css and html

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+// console.log('\'Allo \'Allo!');
+
+$(function(){
+
+  // $( "#name" ).autocomplete({ appendTo: "#someElem" });
+
+  // $( "#name" ).autocomplete( "option", "appendTo", "#someElem" );
+
+	///////phone imge fade ////////
+  $('#phoneimg').hover(function(){
+   $('#phone').stop().fadeTo(400,1);
+  }, function() {
+    $('#phone').stop().fadeTo(400,0.60);
+  });
+
+  ////////// Gelolocation ////////
+  	 
+})
